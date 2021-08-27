7a05d714ac465ee59794ca7d53aa6b951191600b
sheetal-10 <sheetalp257@gmail.com>
2021-08-27 22:38:18 +0530

    Added: method for getting user first name and validates it using regex

---
+++ src/com/regex/userregistration/Details.java
@@ -0,0 +1,20 @@
+package com.regex.userregistration;
+
+/**
+ * This class is for getters and setters of User Details
+ * @author sheetal
+ * @since 2021-08-26
+ */
+public class Details {
+	//Variable
+	private String firstName;
+
+	// Getters and Setters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
--- src/com/regex/userregistration/MainRunner.java
+++ src/com/regex/userregistration/MainRunner.java
@@ -1,7 +1,20 @@
 package com.regex.userregistration;
 
+/**
+ * This is Main function
+ * Calling methods to get user input for registration and validate
+ * @author sheetal
+ * @since 2021-08-26
+ */
 public class MainRunner {
-	public static void main(String[] args) {
-		System.out.println("Welcome to user registration program");
+		public static void main(String[] args) {
+			//Welcome Message
+			System.out.println("Program for the User Registration and Validation");
+			
+			//Object
+			UserRegistration userRegistration=new UserRegistration();
+			
+			//Calling Methods
+			userRegistration.userFirstName();
 	}
 }
